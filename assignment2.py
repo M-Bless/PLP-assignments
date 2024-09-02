ff61f9c9375db2de6d039c5fe7af78e39a266cca
Blesso <wmugo@kabarak.ac.ke>
2024-09-02 10:54:56 +0300

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+my_list=[]
+my_list=[10,20,30,40]
+my_list.insert(1,15)
+my_list.extend([50,60,70])
+my_list.pop()
+my_list.sort()
+index_of_30=my_list.index(30)
+print("The index of 30 is:",my_list.index(30))
+print("Final list is:",my_list)
